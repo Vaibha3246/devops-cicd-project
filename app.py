6a68de1feb3cfb027c21e09958f79f7bf8e58d3e
Vaibhav Kumar <vaibhavkumar8979581578@gmail.com>
2026-06-20 09:04:22 +0530

    add some route in flask

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return "Hello DevOps Engineer Vaibhav!"
 
+
+@app.route('/version')
+def version():
+    return {
+        "application": "devops-cicd-project",
+        "version": "v2"
+    }
+
+@app.route('/health')
+def health():
+    return {"status": "UP"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
